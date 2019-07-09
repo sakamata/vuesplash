1b29bf598788158d6c543733ae8eed48dc3c93f4
Kazushi Takamura <sakamatapd5@gmail.com>
2019-07-10 05:00:57 +0900

    Add: cap3完了時点 SPA基礎部分ファイルの追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,9 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+// require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +19,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +27,20 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app',
-});
+// const app = new Vue({
+//     el: '#app',
+// });
+
+import Vue from 'vue'
+
+// ルーティングの定義をインポートする
+import router from './router'
+// ルートコンポーネントをインポートする
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  router, // ルーティングの定義を読み込む
+  components: { App }, // ルートコンポーネントの使用を宣言する
+  template: '<App />' // ルートコンポーネントを描画する
+})
---
+++ resources/js/router.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// ページコンポーネントをインポートする
+import PhotoList from './pages/PhotoList.vue'
+import Login from './pages/Login.vue'
+
+// VueRouterプラグインを利用する。
+// これによって<RouterView /> コンポーネント等を使う事ができる
+Vue.use(VueRouter)
+
+// パスとコンポーネントのマッピング
+const routes = [
+    {
+        path: '/',
+        component: PhotoList
+    },
+    {
+        path: '/login',
+        component: Login
+    }
+]
+
+// VurRouterインスタンスを作成する
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
+// VueRouterインスタンスをエクスポートする
+// app.jsでインポートするため
+export default router
